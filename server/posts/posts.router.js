b1bb5b61b5a7d3e65861a57f514826d98c4d130c
Utsavladia <utsavladia@gmail.com>
2024-05-09 21:59:07 +0530

    task 2

--- server/posts/posts.router.js
+++ server/posts/posts.router.js
@@ -1,29 +1,41 @@
 const express = require('express');
+const axios = require('axios');
 const { fetchPosts } = require('./posts.service');
 const { fetchUserById } = require('../users/users.service');
 
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const posts = await fetchPosts();
+  try {
+    const posts = await fetchPosts();
 
-  const postsWithImages = posts.reduce((acc, post) => {
-    // TODO use this route to fetch photos for each post
-    // axios.get(`https://jsonplaceholder.typicode.com/albums/${post.id}/photos`);
-    return [
-      ...acc,
-      {
-        ...post,
-        images: [
-          { url: 'https://picsum.photos/200/300' },
-          { url: 'https://picsum.photos/200/300' },
-          { url: 'https://picsum.photos/200/300' },
-        ],
-      },
-    ];
-  }, []);
+    const postsWithImages = await posts.reduce(async (accPromise, post) => {
+      const acc = await accPromise;
 
-  res.json(postsWithImages);
+      try {
+        const photosResponse = await axios.get(
+          `https://jsonplaceholder.typicode.com/albums/${post.id}/photos`,
+        );
+        const photos = photosResponse.data.map(photo => ({ url: photo.url }));
+
+        return [
+          ...acc,
+          {
+            ...post,
+            images: photos,
+          },
+        ];
+      } catch (error) {
+        console.error('Error fetching photos for post ', post.id, error);
+        return acc; // If there's an error, just return the accumulator without modifying it
+      }
+    }, Promise.resolve([]));
+
+    res.json(postsWithImages);
+  } catch (error) {
+    console.error('Error fetching posts with images ', error);
+    res.status(500).json({ error: 'Error fetching on server' });
+  }
 });
 
 module.exports = router;
